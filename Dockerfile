2a6a59eb4a1b59ce5cbc60e40f2c3ba6f7e2e1fd
Nicolas Degory <ndegory@axway.com>
2017-07-14 15:32:40 -0700

    etcd 3.2.3

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM appcelerator/alpine:3.6.0
 
-RUN apk --update add bind-tools
-ENV ETCD_VERSION 3.2.2
+RUN apk --no-cache add bind-tools
+ENV ETCD_VERSION 3.2.3
 RUN curl -L https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz -o etcd.tar.gz && \
     tar xzf etcd.tar.gz && \
     mv etcd-*/etcd /etcd-*/etcdctl /bin/ && \
@@ -15,6 +15,7 @@ VOLUME /data
 EXPOSE 2379 2380 4001 7001
 
 ENV MIN_SEEDS_COUNT 2
+ENV ETCDCTL_API=3
 
 #HEALTHCHECK --interval=5s --retries=3 --timeout=10s CMD ETCDCTL_API=3 /bin/etcdctl --endpoints=http://127.0.0.1:2379 get ping | grep -q pong
 
--- test.sh
+++ test.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
 version=${1:-latest}
+API=3
 #image=quay.io/coreos/etcd:v$version
 image=appcelerator/etcd:$version
 
 _cleanup() {
-  docker service rm etcdtest >/dev/null 2>&1
+  echo "clean up..."
+  docker service rm etcdtestcluster >/dev/null 2>&1
   docker rm -f etcdtest etcdtestclt >/dev/null 2<&1
   docker network rm etcdtest >/dev/null 2>&1
+  echo "done"
 }
 
+docker network create --attachable -d overlay etcdtest >/dev/null
 echo "Starting etcd single node ($image)"
-#docker run -d -e ETCDCTL_API=3 --name etcdtest $image etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://etcdtest:2379 --listen-peer-urls http://0.0.0.0:2380
-docker run -d -e ETCDCTL_API=3 --name etcdtest $image etcd --advertise-client-urls http://etcdtest:2379
+#docker run -d -e ETCDCTL_API=$API --name etcdtest $image etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://etcdtest:2379 --listen-peer-urls http://0.0.0.0:2380
+docker run -d -e ETCDCTL_API=$API --name etcdtest $image etcd --advertise-client-urls http://etcdtest:2379
 
-sleep 1
+echo -n "health... "
+code=1
+SECONDS=0
+while [[ $code -ne 0 ]]; do
+  docker exec etcdtest etcdctl --endpoints http://localhost:2379 endpoint health | grep -qw healthy
+  code=$?
+  [[ $code -eq 0 ]] && echo "healthy" || echo "unhealthy"
+  [[ $SECONDS -gt 5 ]] && break
+done
+if [ $code -ne 0 ]; then
+  docker exec etcdtest etcdctl --endpoints http://localhost:2379 endpoint health
+  echo "FAIL"
+  _cleanup
+  exit 1
+fi
+
 echo -n "put... "
 docker exec etcdtest etcdctl put hello world
 if [ $? -ne 0 ]; then
@@ -33,35 +52,63 @@ if [ $? -ne 0 ]; then
   _cleanup
   exit 1
 fi
+echo "OK"
 
 echo "single mode test passed"
 docker rm -f etcdtest >/dev/null 2>&1
 
 echo "Starting etcd cluster ($image)"
-#docker run -d -e ETCDCTL_API=3 --name etcdtest $image etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://etcdtest:2379 --listen-peer-urls http://0.0.0.0:2380
-docker network create --attachable -d overlay etcdtest >/dev/null
-docker service create --name etcdtest --replicas 3 --network etcdtest --detach=false -e SERVICE_NAME=etcdtest -e MIN_SEEDS_COUNT=3 $image --advertise-client-urls http://etcdtest:2379
+#docker run -d -e ETCDCTL_API=$API --name etcdtest $image etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://etcdtest:2379 --listen-peer-urls http://0.0.0.0:2380
+docker service create --name etcdtestcluster --replicas 3 --network etcdtest --detach=false -e SERVICE_NAME=etcdtestcluster -e MIN_SEEDS_COUNT=3 $image --advertise-client-urls http://etcdtestcluster:2379
 
-sleep 1
-echo -n "put... "
-docker run --rm -e ETCDCTL_API=3 --name etcdtestclt --network etcdtest $image etcdctl --endpoints http://etcdtest:2379 put hello world
+sleep 5
+
+echo -n "vip... "
+docker network inspect -v etcdtest | grep "\"etcdtestcluster\":"
 if [ $? -ne 0 ]; then
-  echo "failed"
+  echo FAIL
   _cleanup
   exit 1
 fi
 
-docker rm -f etcdtestclt >/dev/null 2>/dev/null
+echo "health... "
+code=1
+SECONDS=0
+while [[ $code -ne 0 ]]; do
+  docker run --rm --network etcdtest -e ETCDCTL_API=$API $image etcdctl --endpoints "http://etcdtestcluster:2379" endpoint health | grep -qw healthy
+  code=$?
+  [[ $code -eq 0 ]] && echo "healthy" || echo "unhealthy"
+  [[ $SECONDS -gt 5 ]] && break
+done
+if [ $code -ne 0 ]; then
+  docker run --rm --network etcdtest -e ETCDCTL_API=$API $image etcdctl --endpoints "http://etcdtestcluster:2379" --debug=true endpoint health
+  docker service ps etcdtestcluster
+  docker service logs etcdtestcluster
+  echo "FAIL"
+  _cleanup
+  exit 1
+fi
+
+echo -n "put... "
+docker run --rm -e ETCDCTL_API=$API --name etcdtestclt --network etcdtest $image etcdctl --endpoints http://etcdtestcluster:2379 put hello world
+if [ $? -ne 0 ]; then
+  echo "FAIL"
+  _cleanup
+  exit 1
+fi
+
 echo -n "get... "
-out=$(docker run --rm -e ETCDCTL_API=3 --name etcdtestclt --network etcdtest $image etcdctl --endpoints http://etcdtest:2379 get hello)
+out=$(docker run --rm -e ETCDCTL_API=$API --name etcdtestclt --network etcdtest $image etcdctl --endpoints http://etcdtestcluster:2379 get hello)
 echo "$out" | grep -wq world
 if [ $? -ne 0 ]; then
   echo
   echo "expected output: world"
   echo "real output: $out"
+  echo "FAIL"
   _cleanup
   exit 1
 fi
+echo "OK"
 
 echo "cluster mode test passed"
 
